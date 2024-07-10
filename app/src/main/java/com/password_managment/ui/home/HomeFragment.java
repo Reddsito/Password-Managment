13c546fd4e806ff4a45b6ed0862e26947050a047
Reddsito <ptyelnegro@gmail.com>
2024-07-09 23:47:42 -0500

    feat(filtro de grupos): Se añadió el filtro por grupos

--- app/src/main/java/com/password_managment/ui/home/HomeFragment.java
+++ app/src/main/java/com/password_managment/ui/home/HomeFragment.java
@@ -5,7 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.SearchView;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,11 +21,13 @@ import com.password_managment.components.adapters.PasswordListAdapter;
 import com.password_managment.databinding.FragmentHomeBinding;
 
 import com.password_managment.models.Password;
+import com.password_managment.models.PasswordGroup;
 import com.password_managment.utils.Functions;
 import com.password_managment.utils.helpers.FragmentHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment implements  PasswordListAdapter.OnPasswordClickListener {
 
@@ -32,17 +37,31 @@ public class HomeFragment extends Fragment implements  PasswordListAdapter.OnPas
     private FragmentHelper fragmentHelper;
     RecyclerView passwordList;
     PasswordListAdapter passwordAdapter;
+    Spinner groups;
+    ArrayAdapter<PasswordGroup> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        groups = binding.spinner;
+
 
         fragmentHelper = new FragmentHelper(getChildFragmentManager(), getActivity());
         viewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
 
+        adapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1 );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        PasswordGroup defaultGroup = new PasswordGroup();
+        defaultGroup.setName("All");
+        adapter.add(defaultGroup);
+        groups.setAdapter(adapter);
+
+
         setupObservers();
         setupListeners();
+
+
 
         return view;
     }
@@ -62,6 +81,18 @@ public class HomeFragment extends Fragment implements  PasswordListAdapter.OnPas
                 binding.passwordList.setAdapter(passwordAdapter);
                 binding.passwordList.setLayoutManager(new LinearLayoutManager(requireActivity()));
             }
+        });
+
+        viewModel.passwordGroups.observe(getViewLifecycleOwner(), passwordGroups -> {
+
+            adapter.clear();
+            PasswordGroup defaultGroup = new PasswordGroup();
+            defaultGroup.setName("All");
+            adapter.add(defaultGroup);
+            adapter.addAll(passwordGroups);
+            adapter.notifyDataSetChanged();
+            binding.spinner.setVisibility(View.VISIBLE);
+            binding.searchBar.setVisibility(View.VISIBLE);
         });
 
         viewModel.loading.observe(getViewLifecycleOwner(), isLoading -> {
@@ -92,6 +123,41 @@ public class HomeFragment extends Fragment implements  PasswordListAdapter.OnPas
                 return false;
             }
         });
+
+        groups.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                PasswordGroup passwordGroup = (PasswordGroup) parent.getItemAtPosition(position);
+                getSelectedGroup(passwordGroup);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void getSelectedGroup(PasswordGroup passwordSelected) {
+        ArrayList<Password> passwordList = new ArrayList<>();
+
+        if(Objects.equals(passwordSelected.getName(), "All")) {
+            viewModel.passwords.observe(getViewLifecycleOwner(), passwords -> {
+                passwordAdapter = new PasswordListAdapter(passwords, requireActivity(), this);
+            });
+        } else {
+            viewModel.passwords.observe(getViewLifecycleOwner(), passwords -> {
+                for(Password password: passwords) {
+                    if(Objects.equals(password.getGroupId(), passwordSelected.getId())) {
+                        passwordList.add(password);
+                    }
+                }
+                passwordAdapter = new PasswordListAdapter(passwordList, requireActivity(), this);
+            });
+        }
+
+        binding.passwordList.setAdapter(passwordAdapter);
     }
 
     private void showCreatePasswordFragment() {
